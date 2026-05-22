4259e55395fb267d6280201836439fd81da056fa
Renelle Alemios <villamoralemios123@gmail.com>
2026-05-22 14:32:34 +0800

    Fix profile layout redirecting away when logged in

--- src/app/profile/layout.component.ts
+++ src/app/profile/layout.component.ts
@@ -1,23 +1,9 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-
-import { AccountService } from '@app/_services';
 
 @Component({
-  selector: 'app-account-layout',
+  selector: 'app-profile-layout',
   templateUrl: './layout.component.html',
   standalone: false
 })
 export class LayoutComponent {
-
-  constructor(
-    private router: Router,
-    private accountService: AccountService
-  ) {
-    // redirect to home if already logged in
-    if (this.accountService.accountValue) {
-      this.router.navigate(['/']);
-    }
-  }
-
 }
